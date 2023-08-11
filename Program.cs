556c5b97c1d335ba6fc8f441dc7a50c91ec645db
ahbab259 <hasin_ahbab@yahoo.com>
2023-08-10 21:20:00 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLeetCode
+{
+    class Program
+    {
+        static int LengthOfLastWord(string s)
+        {
+            //string[] words = s.Split(' ');
+            //List<string> wordS = words.Where(c => c.Length == 0).FirstOrDefault().Remove(0,0).ToList();
+            s = s.Trim();
+            List<string> words = s.Split(' ').Where(c=> c.Length > 0).ToList();
+            return words[words.Count() - 1].Length;
+        }
+        static void Main(string[] args)
+        {
+            string s = "Hello  World   ";
+            int result = LengthOfLastWord(s);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
